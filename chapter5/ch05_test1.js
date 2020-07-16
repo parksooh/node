5d816abef7736d8037f473f22c4feebd96e4a3ec
parksooh <parksooh@gmail.com>
2020-07-17 01:59:41 +0900

    문자생성
    res.write("<meta charset=\"UTF-8\">");

--- chapter5/ch05_test1.js
+++ chapter5/ch05_test1.js
@@ -9,7 +9,7 @@ server.listen(port, function(){
 
 server.on('connection', function(socket){
     var addr = socket.address();
-    console.log('클라이언트가 접속했습니다. : %s, %d', add.address, addr.port);
+    console.log('클라이언트가 접속했습니다. : %s , %d', addr.address, addr.port);
 });
 server.on('request', function(req,res){
     console.log('클라이언트 요청이 들어왔습니다.');
@@ -17,4 +17,20 @@ server.on('request', function(req,res){
 });
 server.on('close', function(){
     console.log('서버가 종료됩니다.');
+});
+server.on('request', function(req,res){
+    console.log('클라이언트 요청이 들어왔습니다.');
+    
+    res.writeHead(200,{"content-Type": "text/html; charset=urf-8"});
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<meta charset=\"UTF-8\">");
+    res.write("<title>응답페이지</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("hi>노드제이에스로부터의 응답 페이지<h2>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
 });
